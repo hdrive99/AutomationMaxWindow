2c7c1278fef3c2bca72f3e59eb42f66be37ed139
Harry Di <47988198+hdrive99@users.noreply.github.com>
2022-07-01 14:11:01 +0100

    Main program

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Automation;
+
+namespace Program
+{
+    class Program
+    {
+        private static WindowPattern GetWindowPattern(AutomationElement targetControl)
+        {
+            WindowPattern windowPattern = null;
+
+            try
+            {
+                windowPattern =
+                    targetControl.GetCurrentPattern(WindowPattern.Pattern)
+                    as WindowPattern;
+            }
+            catch (InvalidOperationException)
+            {
+                // Object doesn't support the WindowPattern control pattern
+                return null;
+            }
+            // Make sure the element is usable.
+            if (false == windowPattern.WaitForInputIdle(10000))
+            {
+                // Object not responding in a timely manner
+                return null;
+            }
+            return windowPattern;
+        }
+
+        private static void RestartApplication()
+        {
+            ProcessStartInfo Info = new ProcessStartInfo();
+            Info.Arguments = "/C ping 127.0.0.1 -n 2 && \"" + Process.GetCurrentProcess().MainModule.FileName + "\"";
+            Info.WindowStyle = ProcessWindowStyle.Hidden;
+            Info.CreateNoWindow = true;
+            Info.FileName = "cmd.exe";
+            Console.WriteLine("Initiating restart...");
+            Process.Start(Info);
+
+            Process currentProcess = Process.GetCurrentProcess();
+            var currentProcessId = currentProcess.Id;
+            Process.GetProcessById(currentProcessId).CloseMainWindow();
+            Process.GetProcessById(currentProcessId).Close();
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Application has been started. Now listening to windows...");
+
+            Automation.AddAutomationEventHandler(WindowPattern.WindowOpenedEvent, AutomationElement.RootElement, TreeScope.Children, (sender, e) =>
+            {
+                var element = (AutomationElement)sender;
+                var name = element.Current.Name;
+
+                Console.WriteLine("open: " + name + " hwnd:" + element.Current.NativeWindowHandle);
+
+                // Determine which windows to maximize
+                // Set if(true) to maximize every window, or remove it & set your own conditions in if-statement for which windows to maximize
+                // Multiple name.Contains() conditions with OR operators can be used
+                if (true || name.Contains("Google Chrome")) // Remove true to only maximize Google Chrome or set own conditions
+                {
+                    var windowPattern = GetWindowPattern(element);
+                    windowPattern.SetWindowVisualState(WindowVisualState.Maximized);
+                    Console.WriteLine("Maximized a " + name + " window");
+                }
+
+                Automation.AddAutomationEventHandler(WindowPattern.WindowClosedEvent, element, TreeScope.Element, (s, e2) =>
+                {
+                    Console.WriteLine("close: " + name + " hwnd:" + element.Current.NativeWindowHandle);
+                });
+            });
+
+            /*
+            // Focus Changed Event Handler (for other usage options, disabled to reduce memory usage)
+            Automation.AddAutomationFocusChangedEventHandler((sender, e) =>
+            {
+                var element = (AutomationElement)sender;
+                var name = element.Current.Name;
+                Console.WriteLine("focused: " + name + " hwnd:" + element.Current.NativeWindowHandle);
+            });
+            */
+
+            // 6 hour wait while processing thread to handle maximizing of windows, before a scheduled app restart
+            // Change wait time if desired (parameter is in milliseconds), or exclude number to wait indefinitely (until an error occurs)
+            new ManualResetEvent(false).WaitOne(21600000);
+            Console.WriteLine("Application has closed handlers for some unexpected error or it has been 6 hours. Restarting app...");
+            Automation.RemoveAllEventHandlers();
+            // Automatic restart for when the handlers close unexpectedly, and to clear memory usage buildup
+            RestartApplication();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # AutomationMaxWindow
 Auto-maximizes newly created windows using Automation (to work with FancyZones' current active monitor)
+
+# Requirements
+To run without customizing which windows to maximize, download the .exe and run it. To build the program after customizing options, download .NET Framework 4.8 and change the assembly reference paths in AutomationMaxWindow.csproj if required. This version is required as some UI Automation methods will not work otherwise. 
+
+# What this app is for
+If you are using dual monitors, you may want the functionality of having windows open on the current active monitor (i.e. the monitor where the cursor is). 
+
+FancyZones can do this with the new experimental option "move newly created windows to the current active window", but when it moves the window, it resets the window size (and unmaximizes it) if it was maximized. It currently does not have the option to maximize newly moved windows. 
+
+Program parameters to maximize windows upon opening exist, but they occur before FancyZones moves the window, so a small delay is required. Other third-party programs that handle window behavior did not solve this, so this app does so using the accessibility framework Microsoft UI Automation. 
+
+# Customization
+Multiple conditions can be set for determining which windows should be maximized, instead of the default which maximizes every window. For example, name.Contains("Notepad") will maximize every window that has "Notepad" as a substring in its window title. 
+
+The app also restarts on a schedule, for when the handlers close unexpectedly, and to clear memory usage buildup (which is minor, but still worth clearing). This schedule can be changed by editing the value in WaitOne(21600000), which is the default schedule of 6 hours (in milliseconds). You can exclude the number to wait indefinitely (until an error occurs). If you don't want the app to restart upon crashing or waiting, remove RestartApplication(). 
+
+# Run this app in the background
+Change the output type in AutomationMaxWindow.csproj to winexe instead of exe to run the app in the background, before building the program. You can also remove the Console.WriteLine() lines if you don't want the app to log your window titles in the console. 
